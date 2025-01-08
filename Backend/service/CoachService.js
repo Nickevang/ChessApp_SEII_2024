7fd279b312c5e1a4352ae984180b97bd5c4a613c
Aphrodite Latskou <afroditilatskou@gmail.com>
2025-01-08 23:38:19 +0200

    Reverting some changes

--- Backend/service/CoachService.js
+++ Backend/service/CoachService.js
@@ -1,49 +1,49 @@
 'use strict';
 
+
 /**
- * Retrieves a coach by their ID.
- * coachID - The ID of the coach to retrieve.
- */
-exports.getCoach = function (coachID) {
-  return new Promise(function (resolve, reject) {
+ * Get coach by ID
+ *
+ * coachID Long Coach ID to get
+ * returns Coach
+ **/
+exports.getCoach = function(coachID) {
+  return new Promise(function(resolve, reject) {
     const coaches = {
-      1: { id: 1, name: "Guardiola" },
-      2: { id: 2, name: "Xavi" },
-      3: { id: 3, name: "Flick" },
+      1: {id:1, name: "Guardiola"},
+      2: {id:2, name: "Xavi"},
+      3: {id:3, name: "Flick"}
     };
 
     if (coaches[coachID]) {
-      // Coach exists; resolve with the coach's data
       return resolve(coaches[coachID]);
     } else if (!coaches[coachID]) {
-      // Coach not found; reject with a 404 error code
-      return reject({ code: 404 });
+      return reject({code: 404});
     } else {
-      // Invalid request; reject with a 400 error code
-      return reject({ code: 400 });
+      return reject({code:400})
     }
   });
-};
+}
+
 
 /**
- * Creates a new coach using the provided details.
- * body - JSON object containing the new coach's details.
- */
-exports.postCoach = function (body) {
-  return new Promise(function (resolve, _reject) {
-    const examples = {
-      'application/json': {
-        name: "name",
-        id: 0,
-      },
-    };
-
+ * Create a new coach given their name
+ *
+ * body Coach JSON object with the studentID and groupID
+ * returns Coach
+ **/
+exports.postCoach = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "name" : "name",
+  "id" : 0
+};
     if (Object.keys(examples).length > 0) {
-      // Return the first example object as the response
       resolve(examples[Object.keys(examples)[0]]);
     } else {
-      // Resolve without returning any data if no examples are available
       resolve();
     }
   });
-};
+}
+
